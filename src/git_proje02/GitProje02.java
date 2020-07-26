ad61e14d641efaebed6a8bb57a3e9d282c3cbe21
Ekram <ekremkose2001@gmail.com>
2020-07-25 19:31:00 -0500

    Master conflict

--- src/git_proje02/GitProje02.java
+++ src/git_proje02/GitProje02.java
@@ -5,6 +5,7 @@ public class GitProje02 {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
+		System.out.println("Master'den conflict bir commit");
 
 	}
 
