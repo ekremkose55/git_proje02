c400ab1ef5498c8db20362fbb537a5fd0a697b3c
Ekram <ekremkose2001@gmail.com>
2020-07-25 19:17:19 -0500

    ikinci branch'im olustu

--- src/git_proje02/GitProje02.java
+++ src/git_proje02/GitProje02.java
@@ -5,6 +5,7 @@ public class GitProje02 {
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
 		System.out.println("Second commit from local repository");
+		System.out.println("Yeni branch'im oldu adi feature");
 
 	}
 
