b1ea710beece1e18eb83ef4565cbaf4e11d3c995
Ekram <ekremkose2001@gmail.com>
2020-07-25 19:04:28 -0500

    ikinci commit

--- src/git_proje02/GitProje02.java
+++ src/git_proje02/GitProje02.java
@@ -4,6 +4,7 @@ public class GitProje02 {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local repository");
+		System.out.println("Second commit from local repository");
 
 	}
 
